53be8e16f7394ce30ef1defc97a428cd3502110e
AidanTrent <84699533+AidanTrent@users.noreply.github.com>
2022-03-18 16:27:55 -0600

    Fix improper variable names

--- tgaExample.c
+++ tgaExample.c
@@ -15,18 +15,18 @@ int main(void){
 	struct TGAImg* image = makeImg(0, 0, UNCOMP_RGB, 0, 0, 0, 0, 0, width, height, IMG_PIXEL_SIZE, 0);
 	struct Color color = {0,0,0};
 
-	float redIntermediate = 0;
 	float blueIntermediate = 0;
+	float redIntermediate = 0;
 	float widthInc = (255. / width);
 	float heightInc = (255. / height);
 
 	for (int y = 0; y < height; y++){
-		redIntermediate += heightInc;
-		color.blue = redIntermediate;
-		blueIntermediate = 0;
+		blueIntermediate += heightInc;
+		color.blue = blueIntermediate;
+		redIntermediate = 0;
 		for (int x = 0; x < width; x++){
-			blueIntermediate += widthInc;
-			color.red = blueIntermediate;
+			redIntermediate += widthInc;
+			color.red = redIntermediate;
 			setPixel(image, color, x, y);
 		}
 	}
